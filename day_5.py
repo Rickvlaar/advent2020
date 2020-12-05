2574b7d614b0a1c40cccc1a5a705ad6eb64c9dba
RickForce <Rick@Rickforce.com>
2020-12-05 10:37:54 +0100

    day 5

---
+++ day_5.py
@@ -0,0 +1,14 @@
+import util
+import itertools
+
+seats_file = 'input_files/day_5.txt'
+test_string = 'FBFBBFFRLR'
+
+
+def parse_seat_ids():
+    seats_lists = [line.translate(str.maketrans({'F': '0', 'L': '0', 'B': '1', 'R': '1'})) for line in util.parse_file_as_list(seats_file)]
+    seat_ids = [int(x, 2) for x in seats_lists]
+    my_seat = set(range(80, int('1' * 10, 2))) - set(seat_ids)
+    print(max(seat_ids))
+    print(min(my_seat))
+
