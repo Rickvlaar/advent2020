1dbcf5d9f42776fc0a528ad2833d7c6713de6682
RickForce <Rick@Rickforce.com>
2020-12-09 21:06:29 +0100

    day 9b

--- day_9.py
+++ day_9.py
@@ -1,12 +1,24 @@
 import util
-from itertools import starmap
+from itertools import product, starmap, combinations_with_replacement, accumulate
 number_file = 'input_files/day_9.txt'
+number_list = [int(num) for num in util.parse_file_as_list(number_file)]
 
 
 def find_weakness(preamble=25):
-    number_list = [int(num) for num in util.parse_file_as_list(number_file)]
     for index, num in enumerate(number_list):
         if index > preamble - 1:
             all_possible_permutations = {x + y for x in number_list[index - preamble:] for y in number_list[index - preamble:]}
             if num not in all_possible_permutations:
                 return num
+
+
+def determine_weakness(weak_num):
+    combination_size = 2
+    index = 0
+    while True:
+        for index, value in enumerate(number_list, combination_size):
+            # print(number_list[index: index + combination_size])
+            if sum(number_list[index - combination_size:index]) == weak_num:
+                return min(number_list[index - combination_size:index]) + max(number_list[index - combination_size:index])
+            index += 1
+        combination_size += 1
