76a9a96e1ce273438117dad984a9237dde67793e
RickForce <Rick@Rickforce.com>
2020-12-30 16:56:02 +0100

    day 19

--- day_19.py
+++ day_19.py
@@ -1,5 +1,5 @@
 import util
-import itertools
+import re
 
 test_rules = ['0: 4 1 5',
               '1: 2 3 | 3 2',
@@ -13,6 +13,8 @@ test_rules = ['0: 4 1 5',
               'abbbab',
               'aaabbb',
               'aaaabbb']
+
+twest_rules = util.parse_file_as_list('input_files/day_19_part_2.txt')
 
 the_rules = util.parse_file_as_list('input_files/day_19.txt')
 
@@ -66,18 +68,13 @@ class Node:
             self.right.traverse_tree(node)
 
 
-def build_rules(start_rule_no='0', part_2=False):
+def build_rules(start_rule_no='0'):
     rules_dict = {rule.split(':')[0]: rule.split(':')[1][1:].replace('"', '').split(' ') for rule in the_rules if
                   ':' in rule}
     values_set = set([rule for rule in the_rules if ':' not in rule])
 
-    if part_2:
-        rules_dict['8'] = ['42', '|', '42', '8']
-        rules_dict['11'] = ['42', '31', '|', '42', '11', '31']
-
     root = Node()
     rule_check(start_rule_no, rules_dict, root)
-    root.end_nodes.clear()
     root.traverse_tree()
     allcombos = set()
 
@@ -116,3 +113,64 @@ def rule_check(rule_no, rules_dict, node):
                 else:
                     rule_check(rule, rules_dict, node.add_child(value=rule, go_right=True))
                     sister_node = node.right
+
+
+def build_rules_2(start_rule_no='0', part_2=False):
+
+    rules_dict = {rule.split(':')[0]: rule.split(':')[1][1:].replace('"', '') for rule in the_rules if
+                  ':' in rule}
+    values_set = set([rule for rule in the_rules if ':' not in rule])
+
+    if part_2:
+        rules_dict['8'] = '42 | 42 8'
+        rules_dict['11'] = '42 31 | 42 11 31'
+        # 8 = 42 42 42 42 infinite
+        # 11 = 42 31 or 42 42 42 31 31 31 infinite expanding outwards
+        # start rule = 8 AND 11
+        # minimaal 2* 42, geen max, minimaal 1*31 en minimaal x*31 en x+1*42
+        fodytwo = (regexp_solution('42', rules_dict)).replace(' ', '')
+        dirtyone = (regexp_solution('31', rules_dict)).replace(' ', '')
+
+        matching_rules = set()
+        regexp = '\\b' + fodytwo + fodytwo + dirtyone + '\\b'
+        for value in values_set:
+            if re.search(regexp, value):
+                matching_rules.add(value)
+            else:
+                for x in range(1, 10):
+                    regexp = '\\b' + x * fodytwo + fodytwo + dirtyone + '\\b'
+                    if re.search(regexp, value):
+                        matching_rules.add(value)
+                        break
+                    for y in range(1, 10):
+                        regexp = '\\b' + y * fodytwo + x * fodytwo + x * dirtyone + '\\b'
+                        if re.search(regexp, value):
+                            matching_rules.add(value)
+                            break
+        return matching_rules, regexp
+    else:
+        regexp = '\\b' + (regexp_solution(start_rule_no, rules_dict)).replace(' ', '') + '\\b'
+
+        matching_rules = set()
+        for value in values_set:
+            if re.search(regexp, value):
+                matching_rules.add(value)
+        return matching_rules, regexp
+
+
+def regexp_solution(rule_no, rules_dict):
+    rule = rules_dict.get(rule_no)
+    rule = rule.split(' ')
+
+    new_rule = ''
+    for index, char in enumerate(rule):
+        if char.isdigit():
+            new_rule += regexp_solution(char, rules_dict)
+        else:
+            new_rule += char
+        if index != len(rule) - 1:
+            new_rule += ' '
+    if '|' in rule:
+        new_rule = '(' + new_rule + ')'
+    return new_rule
+
